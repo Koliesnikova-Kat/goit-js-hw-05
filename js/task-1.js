c869954ab6010cf97cb2f3351b91637889894c48
Kateryna <152926745+Koliesnikova-Kat@users.noreply.github.com>
2024-07-16 01:10:54 +0300

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+function isEnoughCapacity(products, containerSize) {
+    const fruits = Object.values(products);
+    let total = 0;
+    for (const fruit of fruits) {
+        total += fruit;
+    }
+    return (total <= containerSize);
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
