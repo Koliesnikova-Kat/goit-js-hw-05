7ab68c8ca469164127326ab7a39e2a9fa169a8c1
Kateryna <koliesnikova.kateryna@gmail.com>
2024-07-21 00:58:44 +0300

    done

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,43 @@
-function isEnoughCapacity(products, containerSize) {
-    const fruits = Object.values(products);
-    let total = 0;
-    for (const fruit of fruits) {
-        total += fruit;
-    }
-    return (total <= containerSize);
-}
+const getUserNames = (users) => {
+    return users.map(user => user.name);
+};
 
-console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
-console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
-console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
-console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
+console.log(
+    getUserNames([
+    {
+        name: "Moore Hensley",
+        email: "moorehensley@indexia.com",
+        balance: 2811
+    },
+    {
+        name: "Sharlene Bush",
+        email: "sharlenebush@tubesys.com",
+        balance: 3821
+    },
+    {
+        name: "Ross Vazquez",
+        email: "rossvazquez@xinware.com",
+        balance: 3793
+    },
+    {
+        name: "Elma Head",
+        email: "elmahead@omatom.com",
+        balance: 2278
+    },
+    {
+        name: "Carey Barr",
+        email: "careybarr@nurali.com",
+        balance: 3951
+    },
+    {
+        name: "Blackburn Dotson",
+        email: "blackburndotson@furnigeer.com",
+        balance: 1498
+    },
+    {
+        name: "Sheree Anthony",
+        email: "shereeanthony@kog.com",
+        balance: 2764
+    },
+])
+); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
--- js/task-2.js
+++ js/task-2.js
@@ -1,36 +1,60 @@
-function calcAverageCalories(days) {
-    let total = 0;
-    if (days.length === 0) {
-        return 0;
+const getUsersWithFriend = (users, friendName) => {
+    return users.filter(user => user.friends.includes(friendName));
+};
+
+const allUsers = [
+    {
+        name: "Moore Hensley",
+        friends: ["Sharron Pace"]
+    },
+    {
+        name: "Sharlene Bush",
+        friends: ["Briana Decker", "Sharron Pace"]
+    },
+    {
+        name: "Ross Vazquez",
+        friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
+    },
+    {
+        name: "Elma Head",
+        friends: ["Goldie Gentry", "Aisha Tran"]
+    },
+    {
+        name: "Carey Barr",
+        friends: ["Jordan Sampson", "Eddie Strong"]
+    },
+    {
+        name: "Blackburn Dotson",
+        friends: ["Jacklyn Lucas", "Linda Chapman"]
+    },
+    {
+        name: "Sheree Anthony",
+        friends: ["Goldie Gentry", "Briana Decker"]
     }
-    for (const day of days) {
-        total += day.calories;
-    }
-    return total / days.length;
-}
+];
 
-console.log(
-    calcAverageCalories([
-        { day: "monday", calories: 3010 },
-        { day: "tuesday", calories: 3200 },
-        { day: "wednesday", calories: 3120 },
-        { day: "thursday", calories: 2900 },
-        { day: "friday", calories: 3450 },
-        { day: "saturday", calories: 3280 },
-        { day: "sunday", calories: 3300 }
-    ])
-); // 3180
+console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
+// [
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
 
-console.log(
-    calcAverageCalories([
-        { day: "monday", calories: 2040 },
-        { day: "tuesday", calories: 2270 },
-        { day: "wednesday", calories: 2420 },
-        { day: "thursday", calories: 1900 },
-        { day: "friday", calories: 2370 },
-        { day: "saturday", calories: 2280 },
-        { day: "sunday", calories: 2610 }
-    ])
-); // 2270
+console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
+// [
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
 
-console.log(calcAverageCalories([])); // 0
+console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []
