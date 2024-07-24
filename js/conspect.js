b7951e2fcbd956f9a1e55b1772110da6e3c01d7f
Kateryna <koliesnikova.kateryna@gmail.com>
2024-07-24 21:37:27 +0300

    finish

---
+++ js/conspect.js
@@ -0,0 +1,28 @@
+// function changeEven(numbers, value) {
+//     const newNumbers = [];
+//     numbers.forEach(element => {
+//         if (element % 2 === 0) {
+//             newNumbers.push(element + value);
+//         } else {
+//             newNumbers.push(element)
+//         }
+//     });
+//     return newNumbers;
+// }
+
+// console.log(changeEven([1, 2, 3, 4, 5], 10));
+
+
+
+
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+
+// const evenNumbers = numbers.filter(number => number % 2);
+// const oddNumbers = numbers.filter(number => !(number % 2));
+
+// console.log(evenNumbers);
+// console.log(oddNumbers);
+
+
+
+
